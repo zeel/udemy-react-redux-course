f69e358a8618044b9c4d6fa05be3dc5a797d8fbe
zeel <kshah215@gmail.com>
2019-04-24 10:51:04 +0530

    merged conflicts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+  }
+
+  componentDidMount() {
+    const currentMonth = new Date().getMonth();
+    if( (currentMonth >= 2) && (currentMonth <= 9) )  {
+      this.setState({text: "Burr, It's Chilly!"});
+    } else {
+      this.setState({text: "Let's hit the beach!"});
+    }
+
+  }
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="season-text">
+          <div>{this.state.text}</div>
+        </div>
       </div>
     );
   }
