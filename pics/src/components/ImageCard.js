0fa376e340bb85db6c39a6de775e2ed0e25d962a
zeel <kshah215@gmail.com>
2019-05-12 13:49:35 +0530

    corrected css for better look

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,35 +1,34 @@
 import React from "react";
+import "./imageCard.css";
 
-const IMAGE_CARD_HEIGHT = 10;
+const IMAGE_CARD_GRID_HEIGHT = 10;
 
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
     this.image = React.createRef();
-    this.label = React.createRef();
     this.state = { rowEnd: 0 };
   }
   componentDidMount() {
     this.image.current.addEventListener("load", this.setSpans);
   }
   setSpans = () => {
-    console.log(this.image.current.clientHeight);
     this.setState({
       rowEnd: Math.ceil(
-        (this.image.current.clientHeight +
-          this.label.current.clientHeight +
-          10) /
-          IMAGE_CARD_HEIGHT
+        (this.image.current.clientHeight + 25) / IMAGE_CARD_GRID_HEIGHT
       )
     });
   };
   render() {
     const { image } = this.props;
     return (
-      <div style={{ gridRowEnd: `span ${this.state.rowEnd}` }}>
-        <label ref={this.label} className="ui label">
-          {image.alt}
-        </label>
+      <div
+        className="image-card"
+        style={{
+          gridRowEnd: `span ${this.state.rowEnd}`,
+          visibility: `${this.state.rowEnd ? "visible" : "hidden"}`
+        }}
+      >
         <img
           className="ui image"
           alt={image.alt}
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -12,11 +12,19 @@ class ImageList extends React.Component {
   //     alt: PropTypes.string
   //   }).isRequired
   // };
+  renderEmptyContainer() {
+    return <div className="ui segment empty-container">No Images Found</div>;
+  }
   render() {
-    const images = this.props.images.map(image => {
+    const images = this.props.images;
+    const imageNodes = this.props.images.map(image => {
       return <ImageCard image={image} key={image.id} />;
     });
-    return <div className="ui segment image-container">{images}</div>;
+    return images.length > 0 ? (
+      <div className="ui segment image-list">{imageNodes}</div>
+    ) : (
+      this.renderEmptyContainer()
+    );
   }
 }
 export default ImageList;
