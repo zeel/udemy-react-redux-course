be82e113888ff8161483abb84ba3b8af1a24b4cd
Zeel Shah <zeel.shah@swiggy.in>
2019-05-27 19:04:23 +0530

    used memoized to solve multiple user request

--- blogs/src/actions/index.js
+++ blogs/src/actions/index.js
@@ -1,14 +1,18 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _memoize from "lodash/memoize";
 
 export const fetchPost = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUserName = userId => async dispatch => {
+const _fetchUser = _memoize(async (userId, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${userId}`);
   dispatch({
     type: "FETCH_USER_NAME",
     payload: { userId, name: response.data.name }
   });
-};
+});
+
+export const fetchUserName = userId => async dispatch =>
+  _fetchUser(userId, dispatch);
--- blogs/src/components/UserName.js
+++ blogs/src/components/UserName.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import { fetchUserName } from "../actions";
 
 class UserName extends React.Component {
