7f0767231fc964b9890c7f9de701e06370d38ff6
Zeel Shah <zeel.shah@swiggy.in>
2019-06-19 15:46:42 +0530

    working translate project

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import Context from "../context";
 export default () => {
+  const val = useContext(Context);
   return (
     <div>
-      <label className="label">Name</label>
+      <label className="label">{val === "english" ? "Name" : "Naam"}</label>
       <input type="text" />
     </div>
   );
